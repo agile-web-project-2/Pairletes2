137e3a4df22f5228caf355067ad4f956233c34da
mseneque <mattseneque@gmail.com>
2017-05-23 14:29:18 +0800

    add mlab

--- test/test-person.js
+++ test/test-person.js
@@ -71,3 +71,58 @@ describe('Person',function(){
    });
  });
 }); 
+
+/*********************
+    TEST REMOTE DB
+**********************/
+
+var mongoose = require('mongoose');
+var dummySchema = new mongoose.Schema({
+    testString: String
+});
+var Dummy = mongoose.model('Dummy', dummySchema, 'dummy');
+
+
+describe('Remote Test Database Connect', function(){
+    before(function(done){//before the test begins
+        var dbURI = 'mongodb://'+mlab.testUser+':'+mlab.testPass+'@ds129651.mlab.com:29651/test-7975';
+        var options = {
+            server: { socketOptions: { keepAlive: 500000, connectTimeoutMS: 50000 } },
+            replset: { socketOptions: { keepAlive: 500000, connectTimeoutMS: 50000 } }
+        };
+        db = mongoose.connect(dbURI, options);
+        done();
+    });
+
+    beforeEach(function(done) {
+        // create dummy instance
+        var dummy_instance = new Dummy({
+            testString: 'cavalry'
+        });
+        // save the new model instance, passing a callback
+        dummy_instance.save(function(error) {
+            if (error) console.log('(saving to database) error ' + error.message);
+            else console.log('          no error - data saved to database');
+            done();
+        });
+    });
+
+    it('should find the remote dummy test string "cavalry"', function(done) {
+        Dummy.findOne({ testString: 'cavalry' }, function(err, dummy) {
+            dummy.testString.should.eql('cavalry');
+            console.log("   testString: ", dummy.testString);
+            done(); //Call done to tell mocha that we are done with this test
+        });
+    });
+
+    afterEach(function(done) {
+        Dummy.remove({}, function() {
+            done();
+        });
+    });
+
+    after(function(done){// after all the test have finished
+        mongoose.connection.close();
+        done();
+    });
+});
